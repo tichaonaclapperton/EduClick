83544303a0af8730fb17d265e6f828a90ee3174f
unknown <clappertontee@gmail.com>
2026-01-03 22:09:36 +0200

    set the files data

---
+++ EduClicks/src/data/afrikaansSyllabus.js
@@ -0,0 +1,43 @@
+export const afrikaansSyllabus = [
+	{
+		term: 1,
+		pdf: "../public/terms/term 1.pdf",
+		topics: [
+			{
+				title: "hhshshshshhshsh",
+				items: ["kkkkkkkkk", "juhytfvtttvt"],
+			},
+		],
+	
+	},
+	{
+		term: 2,
+		pdf: "../public/terms/term 2.pdf",
+		topics: [
+			{
+				title: "hhshshshshhshsh",
+				items: ["kkkkkkkkk", "juhytfvtttvt"],
+			},
+		],
+	},
+	{
+		term: 3,
+		pdf: "../public/terms/term 3.pdf",
+		topics: [
+			{
+				title: "hhshshshshhshsh",
+				items: ["kkkkkkkkk", "juhytfvtttvt"],
+			},
+		],
+	},
+	{
+		term: 4,
+		pdf: "../public/terms/term 4.pdf",
+		topics: [
+			{
+				title: "hhshshshshhshsh",
+				items: ["kkkkkkkkk", "juhytfvtttvt"],
+			},
+		],
+	},
+];
---
+++ EduClicks/src/data/subjects.js
@@ -0,0 +1,48 @@
+export const subjects = [
+	{
+		id: "afr",
+		 name: "Afrikaans",
+		icon: "👩",
+
+		// ✅ TERMS LIVE HERE
+		terms: [
+			{ term: 1, quizzes: [] },
+			{ term: 2, quizzes: [] },
+			{ term: 3, quizzes: [] },
+			{ term: 4, quizzes: [] },
+		],
+	},
+	{
+		id: "math",
+		name: "Mathematics",
+		icon: "☮",
+		terms: [
+			{ term: 1, quizzes: [] },
+			{ term: 2, quizzes: [] },
+			{ term: 3, quizzes: [] },
+			{ term: 4, quizzes: [] },
+		],
+	},
+	{
+		id: "eng",
+		name: "English",
+		icon: "👩‍🔬",
+		terms: [
+			{ term: 1, quizzes: [] },
+			{ term: 2, quizzes: [] },
+			{ term: 3, quizzes: [] },
+			{ term: 4, quizzes: [] },
+		],
+	},
+	{
+		id: "Science",
+		name: "Natural Science",
+		icon: "👩🕎",
+		terms: [
+			{ term: 1, quizzes: [] },
+			{ term: 2, quizzes: [] },
+			{ term: 3, quizzes: [] },
+			{ term: 4, quizzes: [] },
+		],
+	},
+];
