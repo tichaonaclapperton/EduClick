3d3d3f26add2eb5b3706460e956cae3ae0a2acd9
unknown <clappertontee@gmail.com>
2026-01-19 14:32:13 +0200

    added the theme colors

--- EduClicks/src/main.jsx
+++ EduClicks/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import '../src/style/theme.css'
 
 ReactDom.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
